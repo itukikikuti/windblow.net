28dd827795bc181e96778c2917f8e5cadbb69578
中川 <xxvraqwa@gmail.com>
2018-04-11 20:51:29 +0900

    Update utility.js

--- assets/script/utility.js
+++ assets/script/utility.js
@@ -1,17 +1,9 @@
 for (var i = 0; i < document.images.length; i++)
 {
-    var image = document.images[i];
-    var div = document.createElement("div");
-    Wrap(image, div);
-    div.width = image.width;
-    div.height = image.height;
+    document.images[i].class = "modal";
+    baguetteBox.run(".modal");
 }
 function Popup(url)
 {
     window.open(url, "シェア", "width=500,height=500").focus();
-}
-function Wrap(element, wrapper)
-{
-    element.parentNode.before(wrapper);
-    wrapper.appendChild(element);
 }
