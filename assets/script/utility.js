b6b958f396d3263dc15ffd2151ffb56afaff320b
中川 <xxvraqwa@gmail.com>
2018-04-11 21:30:25 +0900

    Update utility.js

--- assets/script/utility.js
+++ assets/script/utility.js
@@ -1,9 +1,20 @@
-for (var i = 0; i < document.images.length; i++)
+vfor (var i = 0; i < document.images.length; i++)
 {
-    document.images[i].class = "modal";
+    var image = document.images[i];
+    var a = document.createElement("a");
+    a.href = image.src;
+    Wrap(image, a);
+    var div = document.createElement("div");
+    div.class = "modal";
+    Wrap(a, div);
     baguetteBox.run(".modal");
 }
 function Popup(url)
 {
     window.open(url, "シェア", "width=500,height=500").focus();
+}
+function Wrap(element, wrapper)
+{
+    element.parentNode.insertBefore(wrapper, element);
+    wrapper.appendChild(element);
 }
