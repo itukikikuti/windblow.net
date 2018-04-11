69e891af71b7fb157fe68325e4d19cc2a2f887d3
中川 <xxvraqwa@gmail.com>
2018-04-11 18:07:28 +0900

    Update utility.js

--- assets/script/utility.js
+++ assets/script/utility.js
@@ -1,4 +1,13 @@
+for (var image in document.images)
+{
+    wrap(image, document.createElement("div"));
+}
 function Popup(url)
 {
     window.open(url, "シェア", "width=500,height=500").focus();
+}
+function Wrap(element, wrapper)
+{
+    element.parentNode.insertBefore(wrapper, element);
+    wrapper.appendChild(element);
 }
