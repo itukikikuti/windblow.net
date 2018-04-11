f54ae7b6034b711da6cb64e8cf4a278c11c7521a
中川 <xxvraqwa@gmail.com>
2018-04-11 22:02:27 +0900

    Update utility.js

--- assets/script/utility.js
+++ assets/script/utility.js
@@ -1,18 +1,19 @@
+var articles = document.getElementsByTagName("article");
+for (var i = 0; i < articles.length; i++)
+{
+    articles[i].className = "modal";
+}
+
 for (var i = 0; i < document.images.length; i++)
 {
     var image = document.images[i];
     var a = document.createElement("a");
     a.href = image.src;
     Wrap(image, a);
-    
-    var articles = document.getElementsByTagName("article");
-    for (var j = 0; j < articles.length; j++)
-    {
-        articles.className = "modal";
-    }
-    
-    baguetteBox.run(".modal");
 }
+    
+baguetteBox.run(".modal");
+
 function Popup(url)
 {
     window.open(url, "シェア", "width=500,height=500").focus();
